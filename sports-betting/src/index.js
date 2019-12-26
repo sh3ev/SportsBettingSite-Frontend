cdddb7a9e936d4b7ba4479f8adedad206217b371
sh3ev <sheevron@gmail.com>
2019-12-27 00:34:00 +0100

    lobbies display

--- sports-betting/src/index.js
+++ sports-betting/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import LobbyList from './components/LobbyList';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const routing = (
+    <Router>
+      <div>
+        <Route path="/" component={App} />
+        <Route path="/lobby" component={LobbyList} />
+      </div>
+    </Router>
+  )
+
+ReactDOM.render(routing, document.getElementById('root'));
